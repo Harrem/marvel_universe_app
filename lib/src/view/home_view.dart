2d913200dbc887c2e847bdf4591b43d3219f4a9f
Harrem <haremmhamad99@gmail.com>
2023-05-31 17:27:24 +0300

    added sliver appBar and sliverList

--- lib/src/view/home_view.dart
+++ lib/src/view/home_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:marvel_universe_app/src/view_model/character_list_vm.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:getwidget/getwidget.dart';
 import '../data/remote/response/status.dart';
 
 final charactersProvider = ChangeNotifierProvider<CharacterListVM>((ref) {
@@ -16,37 +16,151 @@ class HomeView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final value = ref.watch(charactersProvider);
     return Scaffold(
-      appBar: AppBar(title: const Text("MCU")),
       body: value.characters.status == Status.loading
-          ? const Center(child: CircularProgressIndicator())
+          ? const Center(
+              child: GFShimmer(
+              child: Text("loading..."),
+            ))
           : value.characters.status == Status.error
               ? Center(child: Text(value.characters.message!))
-              : ListView.builder(
-                  itemCount: value.characters.data!.results!.length,
-                  itemBuilder: (context, index) {
-                    return ExpansionTile(
-                      title: Text(value.characters.data!.results![index].name
-                          .toString()),
-                      subtitle: Text(
-                          value.characters.data!.results![index].id.toString()),
-                      children: [
-                        ElevatedButton(
-                            onPressed: () {
-                              debugPrint(
-                                  "${value.characters.data!.results![index].thumbnail!.path!}.${value.characters.data!.results![index].thumbnail!.extension!}");
-                            },
-                            child: const Icon(Icons.add)),
-                        Image.network(
-                            "${value.characters.data!.results![index].thumbnail!.path!}.${value.characters.data!.results![index].thumbnail!.extension!}"),
+              : CustomScrollView(slivers: [
+                  SliverAppBar(
+                    backgroundColor: Colors.blueGrey[900],
+                    centerTitle: true,
+                    pinned: true,
+                    expandedHeight: 200,
+                    flexibleSpace: Container(
+                      color: Colors.black,
+                      child: Container(
+                        clipBehavior: Clip.hardEdge,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(30),
+                          ),
+                        ),
+                        child: FlexibleSpaceBar(
+                          centerTitle: true,
+                          background: Image.asset(
+                            'assets/images/bkg1.jpg',
+                            fit: BoxFit.cover,
+                          ),
+                          title: Container(
+                            margin: const EdgeInsets.only(top: 20),
+                            alignment: Alignment.center,
+                            width: 100,
+                            height: 150,
+                            child: Image.asset(
+                              'assets/images/Marvel_Logo_1.png',
+                              fit: BoxFit.fitHeight,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SliverList(
+                    delegate: SliverChildListDelegate(
+                      [
+                        Container(
+                          // color: Colors.grey[900],
+                          height: 150,
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            itemCount: value.characters.data!.results!.length,
+                            itemBuilder: (context, index) => Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: GFAvatar(
+                                radius: 50,
+                                backgroundColor: Colors.blueGrey[900],
+                                backgroundImage: NetworkImage(
+                                  '${value.characters.data!.results![index].thumbnail!.path!}.${value.characters.data!.results![index].thumbnail!.extension!}',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: value.characters.data!.results!.length,
+                          itemBuilder: (context, index) {
+                            return AspectRatio(
+                              aspectRatio: 16 / 9,
+                              child: RoundedCardWidget(
+                                  imageUrl:
+                                      "${value.characters.data!.results![index].thumbnail!.path!}.${value.characters.data!.results![index].thumbnail!.extension!}",
+                                  title: value
+                                      .characters.data!.results![index].name!),
+                            );
+                          },
+                        ),
                       ],
-                    );
-                  },
-                ),
+                    ),
+                  ),
+                ]),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             value.loadCharacters();
           },
           child: const Icon(Icons.refresh)),
+    );
+  }
+}
+
+class RoundedCardWidget extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+
+  const RoundedCardWidget(
+      {super.key, required this.imageUrl, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+              // height: double.infinity,
+              width: double.infinity,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Colors.black.withOpacity(0.8),
+                    Colors.transparent,
+                  ],
+                ),
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
